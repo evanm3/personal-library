4aea9714cbfced6fb6f579f038d28708f5a65ad8
evanm3 <132603114+evanm3@users.noreply.github.com>
2023-11-13 18:29:22 -0700

    add  book to library function

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ class Book {
     constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
-    this.pages = read;
+    this.pages = pages;
+    this.read = read;
     }
 
     info(){
@@ -14,13 +15,29 @@ class Book {
 
 
 function addBookToLibrary() {
-    myLibrary.push(book, ...books);
+    // myLibrary.push(book, ...books);
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").value;
+    let newBook = new Book(title, author, pages, read);
+    console.log(newBook)
     }
 
-let newBook = document.getElementById('new-book-btn');
+let newBookBtn = document.querySelector('#new-book-btn');
+let newBookForm = document.querySelector('#new-book-form')
 
-newBook.addEventListener('click', alert)
 
-function alert(){
-    console.log('this is a test alert');
+newBookBtn.addEventListener('click', form)
+document.querySelector('#new-book-form').addEventListener('submit', submitBook)
+
+function form(){
+    console.log('new book button pressed');
+    newBookForm.style.display = "block";
+}
+
+function submitBook(){
+    event.preventDefault();
+    console.log("submit button pressed");
+    addBookToLibrary()
 }
